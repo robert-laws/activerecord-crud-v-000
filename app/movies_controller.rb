9c07af96e574e7decf8be1b19b2553ac032fab28
Robert Laws <roblaws@me.com>
2017-10-21 07:53:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -11,12 +11,19 @@ def can_be_instantiated_and_then_saved
   movie.save
 end
 
-def can_be_created_with_a_hash_of_attributes(attributes)
+def can_be_created_with_a_hash_of_attributes
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
   movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(title, year)
-  movie = Movie.create do |m|
+  Movie.create do |m|
     m.title = title
     m.release_date = year
   end
@@ -40,7 +47,7 @@ end
 
 def can_find_by_multiple_attributes
   # title == "Title"
-  # release_date == 2000,
+  # release_date == 2000
   # director == "Me"
   Movie.find_by(title: "Title", release_date: 2000)
 end
@@ -48,11 +55,11 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where("release_date > 2002").order(release_date: :desc)
+  Movie.where("release_date > 2000").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
-  # Update the title "Awesome Flick" to "Even Awesomer Flick"
+  # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
